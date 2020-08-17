cb95673fcab687b4566b7e7613cae0a7efa90f8e
gramm <delattre.maxence@gmail.com>
2020-08-17 18:57:17 +0200

    Add footer Add image for about Change website structure Add mailto javascript

---
+++ web/public_html/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer font-small bg-light navbar-light">
+  <div class="container text-center">
+	Website by Maxence Delattre | All rights reserved | <a href="jdramastuff.xyz">jdramastuff.xyz</a> | 
+	
+<script type="text/javascript" language="javascript">
+<!--
+// Email obfuscator script 2.1 by Tim Williams, University of Arizona
+// Random encryption key feature coded by Andrew Moulden
+// This code is freeware provided these four comment lines remain intact
+// A wizard to generate this code is at http://www.jottings.com/obfuscator/
+{ coded = "hgHj@QaOi7iun4HH.pkT"
+  key = "2vbiy3AFpj8Pu5C7TWflt9NhkdR6DQXG4SErKZHVnUMB0aIc1LxoqJsOemzwgY"
+  shift=coded.length
+  link=""
+  for (i=0; i<coded.length; i++) {
+    if (key.indexOf(coded.charAt(i))==-1) {
+      ltr = coded.charAt(i)
+      link += (ltr)
+    }
+    else {     
+      ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+      link += (key.charAt(ltr))
+    }
+  }
+document.write("<a href='mailto:"+link+"'>info@jdramastuff.xyz</a>")
+}
+//-->
+</script><noscript>Mail at "info" on this domain.</noscript>
+
+
+
+  </div>
+</footer>
