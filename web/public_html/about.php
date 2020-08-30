b68baa1ae1b61e78243c2718f568591f4b58fe9b
gramm <delattre.maxence@gmail.com>
2020-08-30 15:37:51 +0200

    fix about.php

--- web/public_html/about.php
+++ web/public_html/about.php
@@ -1,39 +1,24 @@
-<div id="gui" class="container">
-	<div class="row">
-		<div class="col align-self-center">
-		<div class="text-left blabla">
-		<br />
-		<h2>Introduction</h2>
-		<p>
-		Welcome to JDramaStuff! My name is Maxence Delattre, I am a software engineer with a passion for foreign languages. 
-		<br /><br />
-		Originally, I developped a small software I called "LearnByDrama" to count and display word occurences by analyzing its Japanese subtitles.
-		The idea is that I can filter words I already know in order to find the most used words I don't know yet, and focus on learning these words. 
-		By doing this, I avoid spending time on learning words which are almost never used.
-		
-		<br /><br />
-		The software is written in Java, uses Kuromoji as tokenizer, and well, it works but doesn't look very modern:
-		<br /><br />
-		<img src="img/ss_MyDramaWordList.png" />
-		
-		<br /><br />
-		I wanted to share this program eventually, but nowadays it's all about web applications, python libraries and sharing your stuff on github, and so here we are!
-		<br />
-		Hopefully this resource will help you improve your japanese and let you enjoy japanese drama even more !
-		<h2>Sources</h2>
-		The sources of both the website and the python script to parse subtitles are available on GitHub at <a href="https://github.com/gramm/DramaCharCount">https://github.com/gramm/DramaCharCount</a>
-		<br />
-		The subtitles were all gathered from the JPSubbers website, currently at <a href="http://jpsubbers.xyz/">http://jpsubbers.xyz/</a>
-		<br />
-		The JLPT levels were downloaded from <a href="http://www.tanos.co.uk/jlpt/">http://www.tanos.co.uk/jlpt/</a>. Note that there is no official list of kanji or vocabulary, therefore one should not trust
-		this list (or any other) too much.
-		
-		<!--
-		<h2>Tokenization method</h2>
-		The python script relies 
-		-->
-		</p>
-		</div>
-		</div>
-	</div>
-</div>
+<!DOCTYPE html>
+<html>
+<?php
+// Declaration of global variables 
+$page = "about-content.php";
+?>
+
+<body class="d-flex flex-column min-vh-100 bg-light">
+
+<?php
+include('header.php');	
+?>
+
+			<?php
+			include($page);	
+			?>
+
+<?php
+include('footer.php');	
+?>
+</body>
+
+	
+</html>
