2f0200b172fbfb5e245dc6d99f4b6ebeb0e82f00
gramm <delattre.maxence@gmail.com>
2020-10-11 18:16:52 +0200

    docu
    add warning in CleanSubtitles
    update readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DramaCharCount
 A simple script to count characters in all JPSubber subtitle files, and upload the result to a mysql database
+This is a work in progress, in particular the general design will eventually change.
+
+Example of parameters for DramaCharCount.py
+--host=localhost  --user=admin --password=adminpw --database=my_sql_database --path=my_absolute_path_to_subtitles_folder
+
+##DramaCharCount.py
+DramaCharCount counts the occurrence of all kanji in all subtitles files in the provided folder. The script assumes each subfolder is one drama, with the drama name being the subfolder name. 
+All files (including further nested files) will be assumed as belonging to this drama.
+For each kanji, a number of lines (10 in the current implementation) are uploaded to the database to be used as an example for this kanji. The number of lines is shared between all dramas.
+
+##DramaWordCount.py
+DramaWordCount is more or less a copy/paste of DramaCharCount slightly modified to count words instead of characters. It is also implemented as class.
+The counting of words is made by tokenizing lines & getting the normalized form by using sudachipy
+
+
+##ResetTables.py
+The script ResetTables.py will completely erase the DramaCharCount tables and create them anew. Currently this is required when executing other scripts due to unique keys in the database. 
+
+##CleanSubtitles.py
+Warning: in-place editing, no backup is created!
+This script will remove all lines that do not contain any "readable" characters i.e. all non-text lines in a standard SRT file. This is done on all files in all nested subfolders.
+
+##web
+The website source is provided in the web folder.
--- python/CleanSubtitles.py
+++ python/CleanSubtitles.py
@@ -7,22 +7,30 @@ from python.DccUtils import get_subfolders, get_files, parse_args
 def main(argv):
     args = parse_args(argv)
     path = args["path"]
-    subfolders = get_subfolders(path)
 
-    for subfolder in subfolders:
-        for filepath in get_files(subfolder):
-            print("doing {}".format(filepath))
-            with open(filepath, 'r', encoding='utf-8') as file:
-                try:
-                    lines = file.readlines()
-                except:
-                    continue
-            with open(filepath, 'w', encoding='utf-8') as file:
-                for line in lines:
-                    if re.search("[一-龠]+|[ぁ-ゔ]+|[ァ-ヴー]+|[a-zA-Z]+|[ａ-ｚＡ-Ｚ]+|[々〆〤]+", line):
-                        file.write(line)
-                    else:
-                        pass
+    ans = input("WARNING: this will remove all non-readable lines in ALL files in folder & nested folders in {}\n Continue? (y/n)".format(path));
+    while ans != "y" and ans != "n":
+        ans = input("WARNING: this will remove all non-readable lines in ALL files in folder & nested folders in {}\n Continue? (y/n)".format(path));
+    if ans == "n":
+        print("CleanSubtitles canceled")
+        return
+
+#    subfolders = get_subfolders(path)
+#
+#    for subfolder in subfolders:
+#        for filepath in get_files(subfolder):
+#            print("doing {}".format(filepath))
+#            with open(filepath, 'r', encoding='utf-8') as file:
+#                try:
+#                    lines = file.readlines()
+#                except:
+#                    continue
+#            with open(filepath, 'w', encoding='utf-8') as file:
+#                for line in lines:
+#                    if re.search("[一-龠]+|[ぁ-ゔ]+|[ァ-ヴー]+|[a-zA-Z]+|[ａ-ｚＡ-Ｚ]+|[々〆〤]+", line):
+#                        file.write(line)
+#                    else:
+#                        pass
 
 
 if __name__ == "__main__":
